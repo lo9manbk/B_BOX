fc1b4ec074b2ce241aec55e6bc243688e28d6d76
lo9manbk <brikilokman@gmail.com>
2026-08-02 11:29:32 -0400

    intial commit

---
+++ myos/drivers/terminal.h
@@ -0,0 +1,12 @@
+#ifndef TERMINAL_H
+#define TERMINAL_H
+
+
+
+void putchar(char c);
+void print(char *str);
+void new_line();
+void clear_screen();
+void scroll_screen();
+#endif
+
---
+++ myos/kernel/kernel.c
@@ -0,0 +1,13 @@
+#include "../drivers/terminal.h"
+
+
+void kernel_main() {
+   clear_screen();
+int i = 0;
+while (i < 40) {
+    print("i\n");
+    i++;
+}
+}
+
+
